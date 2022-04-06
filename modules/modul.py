7e3bce31aa7084473ac2b38c5f61fc4d15c5acb8
bondarea <b0975615510@gmail.com>
2022-04-07 02:04:52 +0300

    first code

---
+++ modules/modul.py
@@ -0,0 +1,14 @@
+def weather(t):
+    if t in range(-50, 51):
+        if t in range(-50, 0):
+            return "It's super cold today. Be sure you dress well!"
+        elif t in range(0, 11):
+            return "It's windy outside, but we sure you will enjoy your day!"
+        elif t in range(11, 31):
+            return "It's time for outdoor walking!"
+        elif t in range(31, 41):
+            return "It's so hot outside!"
+        elif t in range(41, 51):
+            return "Welcome to hell!"
+    else:
+        return "Plaese re-check result in 5 mins"
--- weather.py
+++ weather.py
@@ -1,24 +1,10 @@
-def weather(t):
-    if t in range(-50, 51):
-        if t in range(-50, 0):
-            return "It's super cold today. Be sure you dress well!"
-        elif t in range(0, 11):
-            return "It's windy outside, but we sure you will enjoy your day!"
-        elif t in range(11, 31):
-            return "It's time for outdoor walking!"
-        elif t in range(31, 41):
-            return "It's so hot outside!"
-        elif t in range(41, 51):
-            return "Welcome to hell!"
-    else:
-        return "Plaese re-check result in 5 mins"
-
+import modules.modul as m
 
 try:
     with open("./inbox/input.txt", 'r', encoding='utf-8') as file:
         temp = file.read()
     temp = int(temp[0:-1])
-    f = weather(temp)
+    f = m.weather(temp)
     print(f)
 except Exception:
     print("Plaese re-check result in 5 mins")
